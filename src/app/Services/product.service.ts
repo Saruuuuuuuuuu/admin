773a67721afc2fdc34bd962fe47df75344dfbcf8
Saruuuuuuuuu <channelofhbgamer@gmail.com>
2025-01-27 13:35:25 +0800

    anhaa

---
+++ src/app/Services/product.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  constructor() {}
+
+  getProducts() {
+    return [
+      {
+        id: 1000,
+        name: 'James Butt',
+        country: {
+          name: 'Algeria',
+          code: 'dz',
+        },
+        company: 'Benton, John B Jr',
+        date: '2015-09-13',
+        status: 'unqualified',
+        verified: true,
+        activity: 17,
+        representative: {
+          name: 'Ioni Bowcher',
+          image: 'ionibowcher.png',
+        },
+        balance: 70663,
+      },
+    ];
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
+import { ProductService } from './Services/product.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    ButtonModule,
+    AvatarModule,
+    AvatarGroupModule,
+    TableModule,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  products!: any[];
+
   title = 'admin';
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.products = this.productService.getProducts();
+  }
 }
